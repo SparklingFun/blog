88fbb8fc5f29efc6f53fcffb36ca198ea06f3596
Sparkling <sparkinglemon@foxmail.com>
2020-08-04 02:50:10 +0800

    online: pub share qrcode

--- themes/icarus/layout/custom/share.jsx
+++ themes/icarus/layout/custom/share.jsx
@@ -10,6 +10,8 @@ const { Component, Fragment } = require('inferno');
 module.exports = class extends Component {
     render() {
         const js = `document.addEventListener('DOMContentLoaded', function () {
+            // functions
+            // have bugs...
             function copyToClipboard(text) {
                 if (text === "") return;
                 var ipt = document.createElement('input')
@@ -40,15 +42,51 @@ module.exports = class extends Component {
                 } else {
                     ipt.select()
                 }
-                window.document.execCommand('Copy')
+                document.execCommand('Copy')
                 ipt.blur()
                 document.body.removeChild(ipt)
                 ipt = null
             }
-            var canvas = document.getElementById('canvas');
+
+            // Event Listeners
+            // QRCode
+            var shareQrcodeSwitcher = document.getElementById('_tool-share-qrcode-switcher');
+            var shareQrcodeBox =  document.getElementById('_tool-share-qrcode-box');
+            var shareQrcodeSwitcherStatus = shareQrcodeSwitcher.dataset.status;
+            var __shortText__ = '';
+            $(shareQrcodeSwitcher).on('click', function() {
+                shareQrcodeSwitcherStatus = !shareQrcodeSwitcherStatus
+                if(shareQrcodeSwitcherStatus) {
+                    shareQrcodeBox.style.display = "block";
+                } else {
+                    shareQrcodeBox.style.display = "none";
+                }
+            })
+            // Copy Link
+            var shareQrcodeCopy = document.getElementById('_tool-share-qrcode-copy');
+            var copiedFlag = false;
+            $(shareQrcodeCopy).on('click', function() {
+                if(__shortText__ && !copiedFlag) {
+                    copyToClipboard('https://arcto.xyz/s/'+__shortText__);
+                    copiedFlag = true;
+                    shareQrcodeCopy.text = "已成功复制链接~";
+                    setTimeout(function() {
+                        copiedFlag = false;
+                        shareQrcodeCopy.text = "复制分享链接";
+                    }, 300);
+                }
+            })
+
+            // main
+            var canvas = document.getElementById('_tool-share-qrcode-canvas');
+            var container = document.getElementById('_tool-share-qrcode-wrapper');
             // 基于短链接服务生成短链
             $.get('https://arcto.xyz/create?url=' + encodeURIComponent(window.location.href), function(data) {
-                if (data.data.code === -1) console.error(data.data.data.msg);
+                if (data.data.code === -1) {
+                    console.error(data.data.data.msg);
+                    container.style.display = "none";
+                }
+                __shortText__ = data.data.data.short;
                 // 正常情况均会返回URL（无论新的或是已存在的）
                 QRCode.toCanvas(canvas, 'https://arcto.xyz/s/' + data.data.data.short, function (error) {
                     if (error) console.error(error);
@@ -56,9 +94,11 @@ module.exports = class extends Component {
             })
         });`;
         return <Fragment>
-            <aside class="_tool-share">
-                <span></span>
-                <canvas id="canvas"></canvas>
+            <aside class="_tool-share" id="_tool-share-qrcode-wrapper">
+                <span id="_tool-share-qrcode-switcher" data-status={false}>二维码分享</span><span id="_tool-share-qrcode-copy" style="display: none;">复制分享链接</span>
+                <figure id="_tool-share-qrcode-box" style="display: none;">
+                    <canvas id="_tool-share-qrcode-canvas"></canvas>
+                </figure>
             </aside>
             {/* <script src="https://unpkg.com/axios@0.19.2/dist/axios.min.js" defer={true}></script> */}
             <script src="https://unpkg.com/qrcode@1.4.4/build/qrcode.min.js" defer={true}></script>
